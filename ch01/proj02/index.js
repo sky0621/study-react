a980826e3abf1f00b466bd472675025244fbd380
sky0621 <sky14work@gmail.com>
2017-07-31 01:04:54 +0900

    ch01/proj02

---
+++ ch01/proj02/index.js
@@ -0,0 +1,11 @@
+const colors = require('colors')
+
+const s1 = "RED"
+const s2 = "BLUE"
+const s3 = "RAINBOW"
+const s4 = "RED"
+
+console.log(s1.underline.red)
+console.log(s2.inverse.blue)
+console.log(s3.rainbow)
+console.log(s4.inverse.red)
